4938b18e8fddad8e657bebe22d39ed58332b5fa1
Sanghyuk Jeong <jade@indentcorp.com>
2025-01-10 11:50:09 +0900

    django_extensions 설치

--- play_with_gql/settings.py
+++ play_with_gql/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     "play_with_gql.library",
     "play_with_gql.users",
 ]
