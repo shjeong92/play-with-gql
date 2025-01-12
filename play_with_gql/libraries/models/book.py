8e824edb05d44f191c6a092e6e20482e96199ea2
Sanghyuk Jeong <jade@indentcorp.com>
2025-01-12 16:51:14 +0900

    마이그레이션 픽스

--- play_with_gql/libraries/models/book.py
+++ play_with_gql/libraries/models/book.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Book(models.Model):
-    library = models.ForeignKey("libraries.Library", on_delete=models.CASCADE)
+    library = models.ForeignKey("libraries.Library", on_delete=models.CASCADE, related_name="books")
     title = models.CharField(max_length=255)
-    author = models.ForeignKey("libraries.Author", on_delete=models.CASCADE)
+    author = models.ForeignKey("libraries.Author", on_delete=models.CASCADE, related_name="books")
     published_date = models.DateField()
