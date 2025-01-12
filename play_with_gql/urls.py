069c8448c23068aea326d5b7cfb0e6186bc63e34
Sanghyuk Jeong <jade@indentcorp.com>
2025-01-12 22:47:28 +0900

    graphql API 등록

--- play_with_gql/urls.py
+++ play_with_gql/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from strawberry.django.views import AsyncGraphQLView
+
+from .schema import schema
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("graphql/", csrf_exempt(AsyncGraphQLView.as_view(schema=schema))),
 ]
